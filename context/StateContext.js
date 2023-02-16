655ed7d493873b86042914e137edb2b184d59ee0
Yuvadi29 <adityatrivedi612@gmail.com>
2023-02-16 21:32:38 +0530

    Setting Up Context for Website

---
+++ context/StateContext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { Toast } from 'react-hot-toast'; //Pop up notification on addition of item in cart
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+    const [showCart, setShowCart] = useState(false);
+    const [cartItems, setCartItems] = useState();
+    const [totalPrice, setTotalPrice] = useState();
+    const [totalQuantities, setTotalQuantities] = useState();
+    const [qty, setQty] = useState(1);
+
+    return (
+        <Context.Provider
+            value={{
+                showCart,
+                cartItems,
+                totalPrice,
+                totalQuantities,
+                qty,
+            }}
+        >
+            {children}
+
+        </Context.Provider>
+    )
+
+}
